34a44b5bdb3ec9f6be1bd44224a9291ad0a7a21f
jadu <jacobdu.official@gmail.com>
2021-02-25 18:18:12 -0800

    test file being used

---
+++ test.py
@@ -0,0 +1,10 @@
+import analysis as an
+import matplotlib.pyplot as plt
+
+filepath = "E:\\Flow\\flow-interface-project\\Testing\\data\\"
+files = ['Specimen_001_BM3 Rag 849 Full Stain Thy_004.fcs']
+comp = "compensation_matrix.csv"
+session = an.Analysis()
+session.importdata(filepath, files, comp)
+ax = session.generatefigure('Specimen_001_BM3 Rag 849 Full Stain Thy_004.fcs', 'FSC-H', 'FSC-A', logicle = False)
+plt.show()
