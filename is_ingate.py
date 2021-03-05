17cd4944e85302a80270f4d832e5008349df3366
eromer10 <78670345+eromer10@users.noreply.github.com>
2021-03-05 13:23:04 -0800

    Update is_ingate.py

--- is_ingate.py
+++ is_ingate.py
@@ -1,40 +1,43 @@
 def point_in_poly(x,y,poly):
-
+   inside = False
    # check if point is a vertex
-   if (x,y) in poly: print("IN")
+   if (x,y) in poly: 
+       
+       inside = True
 
    # check if point is on a boundary
-   for i in range(len(poly)):
-      p1 = None
-      p2 = None
-      if i==0:
-         p1 = poly[0]
-         p2 = poly[1]
-      else:
-         p1 = poly[i-1]
-         p2 = poly[i]
-      if p1[1] == p2[1] and p1[1] == y and x > min(p1[0], p2[0]) and x < max(p1[0], p2[0]):
-         print("IN")
+   if inside == False:
+       for i in range(len(poly)):
+          p1 = None
+          p2 = None
+          if i==0:
+             p1 = poly[0]
+             p2 = poly[1]
+          else:
+             p1 = poly[i-1]
+             p2 = poly[i]
+          if p1[1] == p2[1] and p1[1] == y and x > min(p1[0], p2[0]) and x < max(p1[0], p2[0]):
+             
+             inside = True
 
    n = len(poly)
-   inside = False
-
    p1x,p1y = poly[0]
-   for i in range(n+1):
-      p2x,p2y = poly[i % n]
-      if y > min(p1y,p2y):
-         if y <= max(p1y,p2y):
-            if x <= max(p1x,p2x):
-               if p1y != p2y:
-                  xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-               if p1x == p2x or x <= xints:
-                  inside = not inside
-      p1x,p1y = p2x,p2y
+   if inside == False: 
+       for i in range(n+1):
+          p2x,p2y = poly[i % n]
+          if y > min(p1y,p2y):
+             if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                   if p1y != p2y:
+                      xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                   if p1x == p2x or x <= xints:
+                      inside = not inside
+          p1x,p1y = p2x,p2y
 
    if inside: print ("IN")
    else: print ("OUT")
-poly = [(0, 0), (2, 7), (4, 0), (2, 4)]
-point_in_poly(1, 3, poly)
-point_in_poly(2, 3, poly)
-point_in_poly(4, 2, poly)
-point_in_poly(2, 5, poly)
+poly = [(10, 3), (2, 2), (9, 4), (8, 8), (6, 1), (10, 0), (6, 4)]
+point_in_poly(6, 1, poly)
+point_in_poly(5, 5, poly)
+point_in_poly(7, 5, poly)
+point_in_poly(9, 1, poly)
