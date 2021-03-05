2e82b9ea9ce832074b2c747c432f87fb56dd6e8a
eromer10 <78670345+eromer10@users.noreply.github.com>
2021-03-05 13:07:03 -0800

    Update is_ingate.py

--- is_ingate.py
+++ is_ingate.py
@@ -1,30 +1,40 @@
-from math import tan, pi
-import matplotlib.pyplot as plt 
-verticeslist = [(1, 9), (1, 1), (7, 9), (7, 1)]
-# point1 = (3, 4)
-# point2 = (5,7)
-# point3 = (9,1)
-# point4 = (3,7)
-# n_sides = 4
-def is_ingate(point, vertices):
-    result = False
-    n = len(vertices)
-    vertex = list(vertices[0])
-    p1x = int(vertex[0])
-    p1y = int(vertex[1])
-    for a in range (n+1):
-        vertex = list(vertices[a%n])
-        p2x = int(vertex[0])
-        p2y = int(vertex[1])
-        if point[1] > min(p1y,p2y):
-            if point[0] <= max(p1x,p2x):
-                if p1y != p2y:
-                    xinters = (point[1]-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                if p1x == p2x or point[0] <= xinters:
-                    result = not result
-        p1x,p1y = p2x,p2y
-    print(result)
-is_ingate((5,7), verticeslist)
-is_ingate((5,6), verticeslist)
-is_ingate((5,5), verticeslist)
-is_ingate((4,3), verticeslist)
+def point_in_poly(x,y,poly):
+
+   # check if point is a vertex
+   if (x,y) in poly: print("IN")
+
+   # check if point is on a boundary
+   for i in range(len(poly)):
+      p1 = None
+      p2 = None
+      if i==0:
+         p1 = poly[0]
+         p2 = poly[1]
+      else:
+         p1 = poly[i-1]
+         p2 = poly[i]
+      if p1[1] == p2[1] and p1[1] == y and x > min(p1[0], p2[0]) and x < max(p1[0], p2[0]):
+         print("IN")
+
+   n = len(poly)
+   inside = False
+
+   p1x,p1y = poly[0]
+   for i in range(n+1):
+      p2x,p2y = poly[i % n]
+      if y > min(p1y,p2y):
+         if y <= max(p1y,p2y):
+            if x <= max(p1x,p2x):
+               if p1y != p2y:
+                  xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+               if p1x == p2x or x <= xints:
+                  inside = not inside
+      p1x,p1y = p2x,p2y
+
+   if inside: print ("IN")
+   else: print ("OUT")
+poly = [(0, 0), (2, 7), (4, 0), (2, 4)]
+point_in_poly(1, 3, poly)
+point_in_poly(2, 3, poly)
+point_in_poly(4, 2, poly)
+point_in_poly(2, 5, poly)
